c5d4127b6132ac5e1cc2b0ac613e39f583dd7793
rliaudanskyte <rliaudanskyte@gmail.com>
2022-12-19 13:44:14 +0200

    DataTransferService.java now parses data and converts to Employees

--- src/main/java/com/liaudanskyte/DataTransferService.java
+++ src/main/java/com/liaudanskyte/DataTransferService.java
@@ -1,17 +1,30 @@
 package com.liaudanskyte;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DataTransferService {
 
-    static List<String> readDataLines() {
+    static List<Employee> readDataLines() {
         try {
             var reader = new BufferedReader(new FileReader("./src/main/resources/employeeData.csv"));
-            return reader.lines().collect(Collectors.toList());
+            return reader.lines().map(DataTransferService::convertStringToEmployee).toList();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    static Employee convertStringToEmployee(String line) {
+        List<String> elements = Arrays.asList(line.split(","));
+        if (elements.get(0).equals(ContractType.FIXED.toString())) {
+            return new Employee(ContractType.valueOf(elements.get(0)), elements.get(1), Integer.valueOf(elements.get(2)));
+        } else if (elements.get(0).equals(ContractType.FLEXIBLE.toString())) {
+            return new EmployeeFlexible(ContractType.valueOf(elements.get(0)), elements.get(1), Integer.parseInt(elements.get(2)), Integer.parseInt(elements.get(3)));
+        } else {
+            return new Employee();
         }
     }
 
